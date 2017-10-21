1ac6910df172d29f5010ec5b11f110985b6bd782
NEXTWIN\Usha <gmanoj.varma92@gmail.com>
2017-10-21 18:32:58 +0530

    Add GET /todos Route

--- server/server.js
+++ server/server.js
@@ -17,7 +17,17 @@ app.post('/todos', (req, res) => {
         (doc) => {
             res.send(doc);
         }, (err) => {
-            res.send(err);
+            res.status(400).send(err);
+        }
+    )
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then(
+        (todos) => {
+            res.send({todos});
+        },(err) => {
+            res.status(400).send(err);
         }
     )
 });
